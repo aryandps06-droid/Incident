f84469819db65a74109f90b5adacbb1164a0952e
aryandps06-droid <aryandps06@users.noreply.github.com>
2026-08-08 15:50:23 +0530

    fix: remove custom cursor glow per user request

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,11 @@ import { MedicalIDModal } from './components/modals/MedicalIDModal';
 import { ContactsModal } from './components/modals/ContactsModal';
 import { CPRModal } from './components/modals/CPRModal';
 
-import { CursorGlow } from './components/common/CursorGlow';
-
 export function App() {
   const [splashDone, setSplashDone] = useState(false);
 
   return (
     <>
-      <CursorGlow />
       <AnimatePresence>
         {!splashDone && <SplashScreen onDone={() => setSplashDone(true)} />}
       </AnimatePresence>
